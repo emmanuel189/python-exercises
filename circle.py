ec992148d2bd793eb7ccab48eef9675463bc6411
Mario Garcia <iscmariog@gmail.com>
2016-08-03 12:13:20 -0500

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,6 @@
 # Area of a circle
 # Formula: pi * (r ** 2)
-r = int(input("Introduce el radio del circulo: "))
+r = int(input("Introduce the radius of the circle: "))
 pi = 3.1416
 area = pi * (r ** 2)
-print("El area del circulo es: ", area)
+print("The area of the circle is: ", area)

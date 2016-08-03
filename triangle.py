5f0f0accbc0aec88312d60ceee53e3527a489c7c
Mario Garcia <iscmariog@gmail.com>
2016-08-03 12:23:35 -0500

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,6 @@
+# Area of a triangle
+# Formula: (base * height) / 2
+base = int(input("Introduce base: "))
+height = int(input("Introduce height: "))
+area = (base * height) / 2
+print("The area of the triangle is: ", area)

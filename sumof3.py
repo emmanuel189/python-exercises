e14e8d93bf27cc88eead52597b290a299d7c772e
Mario Garcia <iscmariog@gmail.com>
2016-08-02 22:29:02 -0500

    Create sumof3.py

---
+++ sumof3.py
@@ -0,0 +1,6 @@
+# Sum of three numbers
+a = int(input(“Introduce a number: ”))
+b = int(input(“Introduce a number: ”))
+c = int(input(“Introduce a number: ”))
+sum = a + b + c
+print(“The sum is: ”, sum)

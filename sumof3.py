9d33452b2fac2b4cff2dea235c83a223731ab432
Mario Garcia <iscmariog@gmail.com>
2016-08-02 23:20:31 -0500

    Update sumof3.py

--- sumof3.py
+++ sumof3.py
@@ -1,6 +1,6 @@
 # Sum of three numbers
-a = int(input(“Introduce a number: ”))
-b = int(input(“Introduce a number: ”))
-c = int(input(“Introduce a number: ”))
+a = int(input("Introduce a number: "))
+b = int(input("Introduce a number: "))
+c = int(input("Introduce a number: "))
 sum = a + b + c
-print(“The sum is: ”, sum)
+print("The sum is: ", sum)

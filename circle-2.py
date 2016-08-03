c08bd9da1315436dee08733cb68642a901881594
Mario Garcia <iscmariog@gmail.com>
2016-08-03 12:11:55 -0500

    Create circle-2.py

---
+++ circle-2.py
@@ -0,0 +1,6 @@
+# Area of a circle
+# Formula: pi * (r ** 2)
+from math import pi
+r = int(input(“Introduce the radius of the circle: ”))
+area = pi * (r ** 2)
+print(“The area of the circle is: ”, area)

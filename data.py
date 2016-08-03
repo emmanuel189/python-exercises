c53c789028071199b5f500da5a0362e866757524
Mario Garcia <iscmariog@gmail.com>
2016-08-03 12:26:02 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+# Print name, age, address
+name = input("Introduce name: ")
+age = int(input("Introduce age: "))
+address = input("Introduce address: ")
+print("Name: ", name)
+print("Age: ", age)
+print("Address: ", address)

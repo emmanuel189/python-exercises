d047b2ce46392981a4e5acde956f44111c5fac4c
Mario Garcia <iscmariog@gmail.com>
2016-08-02 23:19:50 -0500

    Update sumof3-2.py

--- sumof3-2.py
+++ sumof3-2.py
@@ -1,5 +1,5 @@
 # Sum of three numbers
 sum = 0
 for i in range(3):
-    sum += int(input(“Introduce a number: ”))
-print(“The sum is: ”, sum)
+    sum += int(input("Introduce a number: "))
+print("The sum is: ", sum)
